72d166c06ad7cd7a08cbefe867da8fb9bb20388b
kuangshan <kuangshan@sf-express.com>
2017-11-29 22:29:32 +0800

    http_api test successful

--- environment/http_api.py
+++ environment/http_api.py
@@ -8,23 +8,27 @@ Created on Mon Nov 27 00:07:35 2017
 from flask import Flask, render_template, session, json, jsonify
 import random as rnd
 import os
+import webbrowser
+import time
 
 rnd.seed(100)
 
-mapSize = 10
+mapSize = 30
 visibleRange = 2
+refreshTime = 2
+maxParcelNum = 10
 
-app = Flask(__name__,static_url_path='',root_path='D:/Code/SF/pick_up/environment')
+app = Flask(__name__,static_url_path='',root_path=os.getcwd())
 
 def getVisibleBarriers(Barriers, curPos, visibleRange):
     visibleBarriers = []
     for bar in Barriers:
-        if abs(bar['pos']['X']-curPos['X'])<=visibleRange or abs(bar['pos']['Y']-curPos['Y'])<=visibleRange:
+        if abs(bar['pos']['X']-curPos['X'])<=visibleRange and abs(bar['pos']['Y']-curPos['Y'])<=visibleRange:
             visibleBarriers.append(bar)
     return visibleBarriers
     
 def genBarriers(courierPos):
-    barNums =  rnd.randint(5, 15)
+    barNums =  rnd.randint(15, 30)
     Barriers = []
     Sets = [(i,j) for i in range(mapSize) for j in range(mapSize) if i!=courierPos['X'] and j!=courierPos['Y']]
     for i in range(barNums):
@@ -34,10 +38,14 @@ def genBarriers(courierPos):
     return Barriers
 
 def genParcels(Barriers, courierPos, Parcels = []):
-    parcelNums = rnd.randint(5, 15)
+    parcelNums = rnd.randint(3, 8)
     Sets = [(i,j) for i in range(mapSize) for j in range(mapSize) if i!=courierPos['X'] and j!=courierPos['Y']]
     for barrier in Barriers:
-        Sets.remove((barrier['pos']['X'], barrier['pos']['Y']))
+        if (barrier['pos']['X'], barrier['pos']['Y']) in Sets:
+            Sets.remove((barrier['pos']['X'], barrier['pos']['Y']))
+    for parcel in Parcels:
+        if (parcel['pos']['X'], parcel['pos']['Y']) in Sets:
+            Sets.remove((parcel['pos']['X'], parcel['pos']['Y']))
     for i in range(parcelNums):
         parcel = rnd.choice(Sets)
         Sets.remove(parcel)
@@ -55,16 +63,32 @@ def canGo(pos):
 
 def getScore(pos, step):
     Parcels = session['Parcels']
+    newParcels = []
     score = 0
     for parcel in Parcels:
         if pos == parcel['pos']:
             score = parcel['value']
-            Parcels.remove(parcel)
-        parcel['value'] -= step%(mapSize/10)
-        if parcel['value'] == 0:
-            Parcels.remove(parcel)
-    session['Parcels'] = Parcels
+            parcel['value'] = 0
+        else:
+            parcel['value'] -=  1 if step%(mapSize/10)==0 else 0
+        if parcel['value'] > 0:
+            newParcels.append(parcel)
+    session['Parcels'] = newParcels
     return score
+
+def rndGenParcels():
+    curTime = int(time.time())
+    lastTime = int(session['lastTime'])
+    Parcels = session['Parcels']
+#    startTime = int(session['startTime'])
+    if (lastTime!=curTime and (curTime-lastTime)>refreshTime or rnd.randint(1, 6)%2==0) and len(Parcels)<maxParcelNum:
+        Barriers = session['Barriers']
+        courier = session['courier']
+        Parcels = genParcels(Barriers, courier['pos'], Parcels)
+        session['Parcels'] = Parcels
+    session['lastTime'] = curTime
+        
+    
 
 
 @app.route('/', methods=['GET','POST'])
@@ -73,11 +97,14 @@ def index():
     courier = {'pos':{'X':0, 'Y':0}, 'direction':0, 'step': 0, 'score':0}
     Barriers = genBarriers(courier['pos'])
     visibleBarriers = getVisibleBarriers(Barriers, courier['pos'], visibleRange)
-    Parcels = genParcels(Barriers=Barriers, courierPos=courier['pos'])
+    Parcels = genParcels(Barriers=Barriers, courierPos=courier['pos'], Parcels=[])
+    print(len(Barriers), len(visibleBarriers))
     session['courier'] = courier
     session['Barriers'] = Barriers
     session['visibleBarriers'] = visibleBarriers
     session['Parcels'] = Parcels
+    session['startTime'] = int(time.time())
+    session['lastTime'] = session['startTime']
     data['courier'] = courier
     data['Parcels'] = Parcels
     data['visibleBarriers'] = visibleBarriers
@@ -88,7 +115,8 @@ def index():
 def showResult():
     data = {}
     data['courier'] = str(session['courier']).replace('\'', '')
-    data['Barriers'] = str(session['visibleBarriers']).replace('\'', '')
+#    data['Barriers'] = str(session['visibleBarriers']).replace('\'', '')
+    data['Barriers'] = str(session['Barriers']).replace('\'', '')
     data['Parcels'] = str(session['Parcels']).replace('\'', '')
     data['MapSize'] = mapSize
     return render_template('main.html', data = data)
@@ -104,10 +132,11 @@ def goLeft():
     if canGo({'X':courier['pos']['X']-1, 'Y':courier['pos']['Y']}):
         courier['pos']['X'] -= 1
     
-    courier['score'] += getScore(courier['pos'])
+    courier['score'] += getScore(courier['pos'], courier['step'])
     session['courier'] = courier
     visibleBarriers = getVisibleBarriers(Barriers, courier['pos'], visibleRange)
     session['visibleBarriers'] = visibleBarriers
+    rndGenParcels()
     data = {'courier':courier, 'Parcels':session['Parcels'], 'visibleBarriers':visibleBarriers, 'mapSize':mapSize}
     return json.dumps(data)
 
@@ -123,10 +152,11 @@ def goRight():
     if canGo({'X':courier['pos']['X']+1, 'Y':courier['pos']['Y']}):
         courier['pos']['X'] += 1
     
-    courier['score'] += getScore(courier['pos'])
+    courier['score'] += getScore(courier['pos'], courier['step'])
     session['courier'] = courier
     visibleBarriers = getVisibleBarriers(Barriers, courier['pos'], visibleRange)
     session['visibleBarriers'] = visibleBarriers
+    rndGenParcels()
     data = {'courier':courier, 'Parcels':session['Parcels'], 'visibleBarriers':visibleBarriers, 'mapSize':mapSize}
     return json.dumps(data)
 
@@ -142,10 +172,11 @@ def goUp():
     if canGo({'X':courier['pos']['X'], 'Y':courier['pos']['Y']-1}):
         courier['pos']['Y'] -= 1
     
-    courier['score'] += getScore(courier['pos'])
+    courier['score'] += getScore(courier['pos'], courier['step'])
     session['courier'] = courier
     visibleBarriers = getVisibleBarriers(Barriers, courier['pos'], visibleRange)
     session['visibleBarriers'] = visibleBarriers
+    rndGenParcels()
     data = {'courier':courier, 'Parcels':session['Parcels'], 'visibleBarriers':visibleBarriers, 'mapSize':mapSize}
     return json.dumps(data)
 
@@ -161,10 +192,11 @@ def goDown():
     if canGo({'X':courier['pos']['X'], 'Y':courier['pos']['Y']+1}):
         courier['pos']['Y'] += 1
     
-    courier['score'] += getScore(courier['pos'])
+    courier['score'] += getScore(courier['pos'], courier['step'])
     session['courier'] = courier
     visibleBarriers = getVisibleBarriers(Barriers, courier['pos'], visibleRange)
     session['visibleBarriers'] = visibleBarriers
+    rndGenParcels()
     data = {'courier':courier, 'Parcels':session['Parcels'], 'visibleBarriers':visibleBarriers, 'mapSize':mapSize}
     return json.dumps(data)
 
--- environment/test_http_api.py
+++ environment/test_http_api.py
@@ -7,25 +7,148 @@ Created on Tue Nov 28 20:21:13 2017
 
 import flask
 import requests
+import webbrowser
 import numpy as np
+import time
+import codecs
+
+import wx
+import wx.html2
+
+import os
+
+
+
 
 class Map:
     def __init__(self, data):
-        self.MapSize = data['mapSize']
+        self.MapSize = data['MapSize']
         self.Parcels = data['Parcels']
-        self.Courier = data['Courier']
+        self.Courier = data['courier']
+        self.Courier['pos'] = (self.Courier['pos']['X'], self.Courier['pos']['Y'])
         self.visibleBarriers = data['visibleBarriers']
         self.Map = np.zeros((self.MapSize, self.MapSize), dtype=int)
         self.Barriers = []
         for bar in self.visibleBarriers:
+            bar['pos'] = (bar['pos']['X'], bar['pos']['Y'])
             self.Barriers.append(bar)
             self.Map[bar['pos']] = -1
         for parcel in self.Parcels:
+            parcel['pos'] = (parcel['pos']['X'], parcel['pos']['Y'])
             self.Map[parcel['pos']] = parcel['value']
+        self.browser = webbrowser.get()
+        self.isOpen = False
+        self.showMap()
+#    
     
-    def show()
+    def showMap(self):
+        rsp = session.get(url + '/showResult')
+        data = rsp.text
+        f = codecs.open('static/result.html', 'w', 'utf-8')
+        f.write(data)
+        f.close()
+        self.isOpen = self.browser.open(os.getcwd()+'\\static\\result.html', new=0, autoraise=False) if not self.isOpen else True
+        
+    def refresh(self, data):
+        self.Parcels = data['Parcels']
+        self.Courier = data['courier']
+        self.Courier['pos'] = (self.Courier['pos']['X'], self.Courier['pos']['Y'])
+        self.visibleBarriers = data['visibleBarriers']
+        self.Map = np.zeros((self.MapSize, self.MapSize), dtype=int)
+        self.Barriers = []
+        for bar in self.visibleBarriers:
+            bar['pos'] = (bar['pos']['X'], bar['pos']['Y'])
+            self.Barriers.append(bar)
+            self.Map[bar['pos']] = -1
+        for parcel in self.Parcels:
+            parcel['pos'] = (parcel['pos']['X'], parcel['pos']['Y'])
+            self.Map[parcel['pos']] = parcel['value']
+        self.showMap()
+        
+    def canGo(self, direction):
+        nextPos = list(self.Courier['pos'])
+        if direction == 0:
+            nextPos[1] += 1
+        elif direction == 1:
+            nextPos[0] -= 1
+        elif direction == 2:
+            nextPos[0] += 1
+        elif direction == 3:
+            nextPos[1] -= 1
+        if -1 in nextPos or self.MapSize in nextPos or self.Map[tuple(nextPos)]==-1:
+            return False
+        else:
+            return True
+        
+#        
+        
+class MyBrowser(wx.Dialog):
+    def __init__(self, *args, **kwds):
+        wx.Dialog.__init__(self, *args, **kwds)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        self.browser = wx.html2.WebView.New(self)
+        sizer.Add(self.browser, 1, wx.EXPAND, 10)
+        self.SetSizer(sizer)
+        self.SetSize((1000, 1000))
 
 if __name__=='__main__':
+#    app = wx.App()
+    url = 'http://127.0.0.1:5000'
+    go_url = {0:'/down', 1:'/left', 2:'/right', 3:'/up'}
     session = requests.session()
-    rsp = session.get('http://127.0.0.1:5000')
+    rsp = session.get(url)
     data = rsp.json()
+    myMap = Map(data)
+    direction = myMap.Courier['direction']
+    for i in range(500):
+        while True:
+#            if myMap.canGo(direction):
+#                break
+            if direction == 0:
+                if myMap.canGo(1):
+                    direction = 1
+                elif myMap.canGo(0):
+                    direction = 0
+                elif myMap.canGo(2):
+                    direction = 2
+                elif myMap.canGo(3):
+                    direction = 3
+                break
+            elif direction == 1:
+                if myMap.canGo(3):
+                    direction = 3
+                elif myMap.canGo(1):
+                    direction = 1
+                elif myMap.canGo(0):
+                    direction = 0
+                elif myMap.canGo(2):
+                    direction = 2
+                break
+            elif direction == 2:
+                if myMap.canGo(0):
+                    direction = 0
+                elif myMap.canGo(2):
+                    direction = 2
+                elif myMap.canGo(3):
+                    direction = 3
+                elif myMap.canGo(1):
+                    direction = 1
+                break
+            elif direction == 3:
+                if myMap.canGo(2):
+                    direction = 2
+                elif myMap.canGo(3):
+                    direction = 3
+                elif myMap.canGo(1):
+                    direction = 1
+                elif myMap.canGo(0):
+                    direction = 0
+                break
+            if not direction in range(4):
+                direction = 0
+        rsp = session.get(url+go_url[direction])
+        data = rsp.json()
+        myMap.refresh(data)
+        direction = myMap.Courier['direction']
+        
+        time.sleep(0.7)
